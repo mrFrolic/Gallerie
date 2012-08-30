b787435eeed725dccd5a19ed914d9d49260433c3
denav <francois.denavaut@gmail.com>
2012-08-30 21:11:42 +0200

    begin css

--- fr/menu.inc.php
+++ fr/menu.inc.php
@@ -1,9 +1,9 @@
 <nav class="menu">
 	<article>
-		<a href="./artistes.php"><img src="../img/img.png" alt="artistes" title="artistes"></a>
-		<a href="./expos.php"><img src="../img/img.png" alt="expos" title="expos"></a>
-		<a href="./agenda.php"><img src="../img/img.png" alt="agenda" title="agenda"></a>
-		<a href="./contact.php"><img src="../img/img.png" alt="contact" title="contact"></a>				
+		<a href="./artistes.php"><img src="" alt="artistes" title="artistes"></a>
+		<a href="./expos.php"><img src="" alt="expos" title="expos"></a>
+		<a href="./agenda.php"><img src="" alt="agenda" title="agenda"></a>
+		<a href="./contact.php"><img src="" alt="contact" title="contact"></a>				
 	</article>
 	<article>
 		<ul>
