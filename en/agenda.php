ccbf4229fc6e7d37c0da59e396dee361c8e8efbc
denav <francois.denavaut@gmail.com>
2012-08-31 03:17:11 +0200

    3/4 css done + dossier font

--- en/agenda.php
+++ en/agenda.php
@@ -5,7 +5,7 @@
 <?php include './menu.inc.php' ?>
 
 		<div class="container">
-			<h1>OPENING october 6th 2012</h1>			
+			<h1>OPENING <span style="color:green">october 6th 2012</span></h1>			
 			<p>
 				The Galerie Nicolas Hugo is pleased to announce its first exhibition, « I Ran With Iran », on the occasion of the inauguration of this new contemporary art parisian space. A meeting between four different artists living in London, Paris and Tehran, different techniques and approaches – brought together by this country, with centuries of culture, most of the time badly known and seen by the rest of the World, Iran.<br><br>
 				« Iran’s glory has always been its culture », Richard Nelson Frye an american iranologist, summerize and complexify with these words, the origins and the large puzzle of the iranian culture. With more than eighty ethnic groups with an History of thousands years, from antic Mesopotamia to Islamic Republic, Cyrus, Xerxès, Reza Shah and Khomeini, this culture is rich, diversified and fascinating.<br><br>
--- fr/artistes.php
+++ fr/artistes.php
@@ -7,10 +7,18 @@
 		<div class="container">
 			<p>La Galerie Nicolas Hugo exposera à partir d’Octobre 2012 lors de sa première exposition, les artistes suivants :</p>
 			<aside>
-				<a href="./_afsoon.php"><img src="../img/img.png" alt="Afsoon" title="Afsoon">AFSOON</a>
-				<a href="./_icy&sot.php"><img src="../img/img.png" alt="Icy & Sot" title="Icy & Sot">ICY & SOT</a>
-				<a href="./_jalalSepehr.php"><img src="../img/img.png" alt="Jalal Sepehr" title="Jalal Sepehr">JALAL SEPEHR</a>
-				<a href="./_lavenChegeni.php"><img src="../img/img.png" alt="Laven Chegeni" title="Laven Chegeni">LAVEN CHEGENI</a>
+				<figure>
+					<a href="./_afsoon.php"><img src="../img/img.png" alt="Afsoon" title="Afsoon">AFSOON</a>
+				</figure>
+				<figure>
+					<a href="./_icy&sot.php"><img src="../img/img.png" alt="Icy & Sot" title="Icy & Sot">ICY & SOT</a>	
+				</figure>
+				<figure>
+					<a href="./_jalalSepehr.php"><img src="../img/img.png" alt="Jalal Sepehr" title="Jalal Sepehr">JALAL SEPEHR</a>	
+				</figure>
+				<figure>
+					<a href="./_lavenChegeni.php"><img src="../img/img.png" alt="Laven Chegeni" title="Laven Chegeni">LAVEN CHEGENI</a>	
+				</figure>
 			</aside>
 		</div>    
 	</div>
